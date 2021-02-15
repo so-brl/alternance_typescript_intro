858accd1b3eb3873a166bfcb1d47dd79aca6a041
so-brl <sonia.brunel@le-campus-numerique.fr>
2021-02-15 13:10:57 +0100

    UPD [INDEX] Choix du nom du personnage

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log('Hello world');
-console.log('Test mpm script restart');
+const prompts = require('prompts');
+
+
+(async () => {
+    const response = await prompts({
+        type: 'text',
+        name: 'Name',
+        message: 'Choisissez le nom de votre personage : '
+    });
+    console.log(response);
+})();
